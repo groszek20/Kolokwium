fff2265e90f214aaa49fedbf8e20f05559d83744
pea <pea@WIN-3JFHFBAUD0K.local>
2016-08-30 20:44:08 +0200

    new commit

---
+++ src/przygotowaniedokolokwium/Biegi.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package przygotowaniedokolokwium;
+
+
+class Biegi extends Sport {
+
+    public Biegi() {
+    }
+
+    
+    
+    public Biegi(String nazwa) {
+        super(nazwa);
+    }
+    
+    public void wypisz(String nazwa){
+        System.out.print(nazwa);
+    }
+}
